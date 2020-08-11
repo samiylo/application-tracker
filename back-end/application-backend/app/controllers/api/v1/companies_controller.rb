e5a6fffda52dfa02a1cdef81ec506fb95cda4d64
samiylo <skryshu@gmail.com>
2020-08-11 14:57:16 -0500

    create Companies Controller

--- back-end/application-backend/app/controllers/api/v1/companies_controller.rb
+++ back-end/application-backend/app/controllers/api/v1/companies_controller.rb
@@ -1,2 +1,45 @@
 class Api::V1::CompaniesController < ApplicationController
+    before_action :set_params, only: [:show, :update, :destroy]
+
+    def index
+        @companies = Company.all
+
+        render json: @companies, status: :ok
+    end
+
+    def show
+        @company = Company.find(params:[:id])
+
+        render json: @company, status: :ok
+    end
+
+    def update
+        @company = Company.find(params:[:id])
+        @company.update(company_params)
+
+        render json: @company, status: :ok
+    end
+
+    def create
+        @company = Company.new(company_params)
+
+        render json: @company, status: :ok
+    end
+
+    def destroy
+        @company = Company.find(params:[:id])
+        @company.delete
+
+        render json: @company, status: :ok
+    end
+
+    private
+
+    def company_params
+        params.require(:company).permit(:name, :adress)
+    end
+
+    def set_params
+        @company = Company.find(params[:id])
+    end
 end
