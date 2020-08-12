81de06811087ad4a2c37191d32d3de8461946182
samiylo <skryshu@gmail.com>
2020-08-12 12:11:57 -0500

    add store

--- front-end/application-frontend/src/index.js
+++ front-end/application-frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux'; //compose combines different middleware.
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './App';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
 
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
 
   document.getElementById('root')
 );
