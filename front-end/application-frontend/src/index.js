a462ebbbc5b50023a4152a9b305f4c6c4057f90e
samiylo <skryshu@gmail.com>
2020-08-12 12:49:19 -0500

    add reducer

--- front-end/application-frontend/src/index.js
+++ front-end/application-frontend/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'; //compose combines different middleware.
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import companyReducer from './reducers/companyReducer'
 import App from './App';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+let store = createStore(companyReducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
 
---
+++ front-end/application-frontend/src/reducers/companyReducer.js
@@ -0,0 +1,5 @@
+// When an action is called, the previous state is passed
+export default function companyReducer(state = {companies: []}, action) {
+
+    return state
+}
